1048718cab0a57b2567a78b457976f1ed256e634
kot14 <43212098+kot14@users.noreply.github.com>
2019-03-22 17:06:25 +0300

    many fixes and add's

--- src/app/services/main-service.service.ts
+++ src/app/services/main-service.service.ts
@@ -5,13 +5,18 @@ import { Subject } from 'rxjs';
   providedIn: 'root'
 })
 export class MainServiceService {
-  private _trigger = new Subject<void>();
+  public points: number;
+  private _type = new Subject<number>();
+  public type$ = this._type.asObservable();
   constructor() { }
 
-  get trigger$() {
-    return this._trigger.asObservable();
+  public type0(t0 = 0) {
+    this._type.next(t0);
   }
-  public triggerOnMyButton() {
-    this._trigger.next();
+  public type1(t1 = 1) {
+    this._type.next(t1);
+  }
+  public type2(t2 = 2) {
+    this._type.next(t2);
   }
 }
--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -9,38 +9,54 @@ import { MainServiceService } from '../services/main-service.service';
 })
 export class Tab1Page {
  public points = 0;
+ public sendpoints = this.points
  public pointsPS = 0;
  public pointsPC = 1;
  public lowCount = 0;
  public medCount = 0;
  public strCount = 0
+ public autoClick = setInterval(() => {this.points += this.pointsPS;}, 1000);
  
 
  
  constructor( private mainService: MainServiceService){}
 
   ngOnInit(){
-    this.mainService.trigger$.subscribe(() => this.getCoinsPS());
+    this.mainService.type$.subscribe(type => {
+      switch(type){
+        case 0:
+          this.pointsPS ++;
+          this.lowCount ++;
+        break;
+        case 1:
+          this.pointsPS ++;
+          this.medCount ++;
+        break;
+        case 2:
+          this.pointsPS ++;
+          this.strCount ++;
+        break
+      }
+    });
   }
 
-
+  set data(value: number){
+    this.mainService.points = value;
+  }
+ 
   gainPoints() {
-  this.points += this.pointsPC;   
-  };   
+    this.points += this.pointsPC;   
+  };
+  
+
+
+
+
   
   getCoinsPS(){
+
     
-  if (this.points >= 3){
-      this.pointsPS += 1;
-      this.points -= 0;
-      setInterval(() => {
-        this.points += this.pointsPS; 
-      }, 1000);
-      this.lowCount ++;
-    }
-    else{
-      alert("Sorry, you don't have enough points.")
-        }
+  
   }
 
 }
